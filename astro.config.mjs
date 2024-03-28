7655ca6f89ec252b2f555b9fd9a450df4cee6708
Ignacio Guerra <ignacio@igguerra.com.ar>
2024-03-28 12:39:02 -0300

    Fundacion Cigesar

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  redirects: {
+    '/prensa': '/medios/',
+    '/somos': '/centro-de-salud-sexual-y-reproductiva/somos/',
+    '/servicios': '/centro-de-salud-sexual-y-reproductiva/servicios/',
+    '/contacto-ar': '/centro-de-salud-sexual-y-reproductiva/contacto/es/',
+    '/contacto-pr': '/centro-de-salud-sexual-y-reproductiva/contacto/pr/',
+    '/vasectomia': '/centro-de-salud-sexual-y-reproductiva/servicios/#vasectomia',
+    '/ginecologia': '/centro-de-salud-sexual-y-reproductiva/servicios/#ginecologia',
+    '/obstetricia': '/centro-de-salud-sexual-y-reproductiva/servicios/#obstetricia',
+    '/psicologia': '/centro-de-salud-sexual-y-reproductiva/servicios/#psicologia',
+    '/capacitaciones': '/centro-de-salud-sexual-y-reproductiva/capacitaciones/',
+  }
+});
---
+++ src/constants/pages.ts
@@ -0,0 +1,51 @@
+interface PageData {
+  slug?: string;
+	title: string;
+  submenu?: Array<PageData>;
+}
+
+const cissyrMenu:Array<PageData> = [
+  {
+    slug: '/centro-de-salud-sexual-y-reproductiva/somos/',
+    title: 'Somos',
+  },
+  {
+    slug: '/centro-de-salud-sexual-y-reproductiva/servicios/',
+    title: 'Servicios',
+  },
+  {
+    slug: '/centro-de-salud-sexual-y-reproductiva/contacto/es/',
+    title: 'Contacto (ES)',
+  },
+  {
+    slug: '/centro-de-salud-sexual-y-reproductiva/contacto/pr/',
+    title: 'Contato (PR)',
+  },
+];
+
+const mainMenu:Array<PageData> = [
+  {
+    slug: '/nosotrxs/',
+    title: 'Nosotrxs',
+  },
+  {
+    slug: '/centro-de-salud-sexual-y-reproductiva/',
+    title: 'CISSYR',
+    submenu: cissyrMenu,
+  },
+  {
+    slug: '/medios/',
+    title: 'Medios',
+  },
+  {
+    slug: '/contacto/',
+    title: 'Contacto',
+  }
+];
+
+export default mainMenu
+export {
+  type PageData,
+  mainMenu,
+  cissyrMenu,
+}
