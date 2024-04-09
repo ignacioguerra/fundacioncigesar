8a2d8e3d32dd1143594d4df8d38ba2ee6799d94e
Ignacio Guerra <ignacio@igguerra.com.ar>
2024-04-09 20:01:26 -0300

    Press url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    '/prensa': '/medios/',
-    '/somos': '/centro-de-salud-sexual-y-reproductiva/somos/',
-    '/servicios': '/centro-de-salud-sexual-y-reproductiva/servicios/',
-    '/contacto-ar': '/centro-de-salud-sexual-y-reproductiva/contacto/es/',
-    '/contacto-pr': '/centro-de-salud-sexual-y-reproductiva/contacto/pr/',
+    '/prensa': '/medios',
+    '/somos': '/centro-de-salud-sexual-y-reproductiva/somos',
+    '/servicios': '/centro-de-salud-sexual-y-reproductiva/servicios',
+    '/contacto-ar': '/centro-de-salud-sexual-y-reproductiva/contacto/es',
+    '/contacto-pr': '/centro-de-salud-sexual-y-reproductiva/contacto/pr',
     '/vasectomia': '/centro-de-salud-sexual-y-reproductiva/servicios/#vasectomia',
     '/ginecologia': '/centro-de-salud-sexual-y-reproductiva/servicios/#ginecologia',
     '/obstetricia': '/centro-de-salud-sexual-y-reproductiva/servicios/#obstetricia',
     '/psicologia': '/centro-de-salud-sexual-y-reproductiva/servicios/#psicologia',
-    '/capacitaciones': '/centro-de-salud-sexual-y-reproductiva/capacitaciones/',
+    '/capacitaciones': '/centro-de-salud-sexual-y-reproductiva/capacitaciones',
   }
 });
